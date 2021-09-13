402ef75e5d74c70b7392a5532aca50e95270bd16
kaiTanobe <stomp0630@icloud.com>
2021-09-13 18:29:50 +0900

    Initial Commit

---
+++ practice_with_YumemiWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_with_YumemiWeather
+//
+//  Created by 田野辺開 on 2021/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
