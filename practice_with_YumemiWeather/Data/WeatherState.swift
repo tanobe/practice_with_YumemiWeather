8b7c9fba7109d1369b61c9814ac3519ab0cff8cb
Tanobe <stomp0630@icloud.com>
2021-11-02 17:47:43 +0900

    Change data file

--- practice_with_YumemiWeather/Data/WeatherState.swift
+++ practice_with_YumemiWeather/Data/WeatherState.swift
@@ -6,9 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
 enum WeatherState: String {
     case sunny = "sunny"
     case rainy = "rainy"
     case cloudy = "cloudy"
 }
+
+extension WeatherState {
+    var image: UIImage? {
+        switch self {
+        case .sunny:
+            return UIImage(named: "sunny")
+        case .cloudy:
+            return UIImage(named: "cloudy")
+        case .rainy:
+            return UIImage(named: "rainy")
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .sunny:
+            return .orange
+        case .cloudy:
+            return .gray
+        case .rainy:
+            return .blue
+        }
+    }
+}
+
