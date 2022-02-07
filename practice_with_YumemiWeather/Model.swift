38fb89881b643df8115ab3b0c3fe95c63a82516f
tanobe <stomp0630@icloud.com>
2022-02-07 13:00:02 +0900

    test push 13:00

--- practice_with_YumemiWeather/Model.swift
+++ practice_with_YumemiWeather/Model.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 import YumemiWeather
 
+
 protocol WeatherViewDelegate: AnyObject {
     func fetchingWeatherSuccessed(weather: Weather)
     func fetchingWeatherFailed(title: String, message: String, action: UIAlertAction)
